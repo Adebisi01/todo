0a76e1cead2c00b600ecdb2bc80df8d9f2625c90
Adebisi01 <oluwafemiadebisi01@gmail.com>
2022-09-21 23:07:18 +0100

    adding completed div

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   faCircleCheck,
   faPencil,
   faTrashAlt,
+  faSquareCheck,
 } from "@fortawesome/free-solid-svg-icons";
 //Default State Value
 const defaultState = {
@@ -57,45 +58,92 @@ const App = () => {
             setShowEdit={setShowEdit}
           />
         ) : null}
-        {state.tasks.map((task) => {
-          return (
-            <div
-              key={task.id}
-              className=" flex flex-1 w-full border-solid border-5 border-blue-200 text-white rounded mt-5 bg-gray-700 py-3"
-            >
-              <button
-                type="submit"
-                className="text-sm text-green-500 mx-1 w-7 b rounded-sm border-solid border-2 border-green-500  "
-                onClick={() => {
-                  dispatch({ type: "COMPLETE", payload: task.id });
-                }}
+        {state.tasks
+          .filter((task) => task.completed !== true)
+          .map((task) => {
+            return (
+              <div
+                key={task.id}
+                className=" flex flex-1 w-full border-solid border-5 border-blue-200 text-white rounded mt-5 bg-gray-700 py-3"
               >
-                {task.completed ? (
-                  <FontAwesomeIcon icon={faCircleCheck} />
-                ) : (
-                  " "
-                )}
-              </button>
-              <p className="text-dark-500 pl-1 capitalize align-left border-solidrounded w-full">
-                {task.task}
-              </p>
-              <button
-                class="text-blue-500 rounded ml-2"
-                title="Edit"
-                onClick={() => updateItem(task.id, task.task)}
+                <button
+                  type="submit"
+                  className="text-sm text-green-500 mx-1 w-7 b rounded-sm border-solid border-2 border-green-500  "
+                  onClick={() => {
+                    dispatch({ type: "COMPLETE", payload: task.id });
+                  }}
+                >
+                  {task.completed ? (
+                    <FontAwesomeIcon icon={faCircleCheck} />
+                  ) : (
+                    " "
+                  )}
+                </button>
+                <p className="text-dark-500 pl-1 capitalize align-left border-solidrounded w-full">
+                  {task.task}
+                </p>
+                <button
+                  class="text-blue-500 rounded ml-2"
+                  title="Edit"
+                  onClick={() => updateItem(task.id, task.task)}
+                >
+                  <FontAwesomeIcon icon={faPencil} />
+                </button>
+                <button
+                  onClick={() => deleteItem(task.id)}
+                  className=" text-red-500 px-3 ml-1 rounded"
+                  title="Delete"
+                >
+                  <FontAwesomeIcon icon={faTrashAlt} />
+                </button>
+              </div>
+            );
+          })}
+        <h1 className=" text-green-700 self-start mt-3 text-lg">
+          <FontAwesomeIcon icon={faSquareCheck} className="pr-2" />
+          Completed Tasks
+        </h1>
+        {state.tasks
+          .filter((task) => task.completed === true)
+          .map((task) => {
+            return (
+              <div
+                key={task.id}
+                className=" flex flex-1 w-full border-solid border-5 border-blue-200 text-white rounded mt-5 bg-gray-700 py-3"
               >
-                <FontAwesomeIcon icon={faPencil} />
-              </button>
-              <button
-                onClick={() => deleteItem(task.id)}
-                className=" text-red-500 px-3 ml-1 rounded"
-                title="Delete"
-              >
-                <FontAwesomeIcon icon={faTrashAlt} />
-              </button>
-            </div>
-          );
-        })}
+                <button
+                  type="submit"
+                  className="text-sm text-green-500 mx-1 w-7 b rounded-sm border-solid border-2 border-green-500  "
+                  onClick={() => {
+                    dispatch({ type: "COMPLETE", payload: task.id });
+                  }}
+                >
+                  {task.completed ? (
+                    <FontAwesomeIcon icon={faCircleCheck} />
+                  ) : (
+                    " "
+                  )}
+                </button>
+                <p className="text-dark-500 pl-1 capitalize align-left border-solidrounded w-full">
+                  {task.task}
+                </p>
+                <button
+                  class="text-blue-500 rounded ml-2"
+                  title="Edit"
+                  onClick={() => updateItem(task.id, task.task)}
+                >
+                  <FontAwesomeIcon icon={faPencil} />
+                </button>
+                <button
+                  onClick={() => deleteItem(task.id)}
+                  className=" text-red-500 px-3 ml-1 rounded"
+                  title="Delete"
+                >
+                  <FontAwesomeIcon icon={faTrashAlt} />
+                </button>
+              </div>
+            );
+          })}
       </div>
     </>
   );
